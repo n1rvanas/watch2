93d103567f9fb8dee0cb3907e8cc4802a8cbd1ea
Vladimir Vysotskyi <n1rvanas@users.noreply.github.com>
2017-07-13 12:29:16 +0300

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,216 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Армейские часы TVG KM-468</title>
+	<base href="{{base_url}}" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=0"/>
+	
+<meta name="description" content="Часы TVG KM-468. Характеристики, фотографии, отзывы. Купить в Киеве с доставкой."/>
+	<meta name="keywords" content="TVG, KM-468, наручные часы ТГВ, LED часы TGV, армейские часы"/>
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=1.0.8" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.51" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1499604420" rel="stylesheet" type="text/css" />
+	<link href="css/1.css?ts=1499604420" rel="stylesheet" type="text/css" />
+	<script src="js/jquery.browser.min.js" type="text/javascript"></script>
+	<link href="js/fancybox/jquery.fancybox-1.3.4.css" rel="stylesheet" type="text/css" />
+	<script src="js/fancybox/jquery.fancybox-1.3.4.pack.js" type="text/javascript"></script>
+	<script type="text/javascript">var currLang = '';</script>
+	<script type="text/javascript">var mainHeaderHeight = 200;</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}
+	<div class="root">
+		<div id="wb_bgs_cont">
+			<div id="wb_page_1_bg"></div>
+		</div>
+		<div id="wb_header_bg"></div>
+		<div id="wb_sbg_placeholder"></div>
+		<div class="vbox wb_container wb_header_fixed" id="wb_header">
+			
+<div id="wb_element_instance0" class="wb_element wb-menu">
+				<ul class="hmenu menu-landing">
+					<li><a href="#" target="_self" title="Главная">Главная</a></li>
+					<li><a href="#description" target="_self" title="Описание">Описание</a></li>
+					<li><a href="#characteristic" target="_self" title="Характеристика">Характеристика</a></li>
+					<li class="active"><a href="#%D0%97%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D1%8C-%D1%81%D0%B5%D0%B9%D1%87%D0%B0%D1%81" target="_self" title="Заказать сейчас">Заказать сейчас</a></li>
+				</ul>
+				<div class="clearfix"></div>
+		</div>
+			<div id="wb_element_instance1" class="wb_element wb_element_picture">
+				<a href="#%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F"><img alt="gallery/m" src="gallery_gen//ec48dd153a7011d1873cef7e408dedde_100x100.png"></a>
+			</div>
+			<div id="wb_element_instance2" class="wb_element" style=" line-height: normal;">
+				<h5 class="wb-stl-subtitle"><a href="http://army-watch.com.ua"></a><a href="http://army-watch.com.ua">Army-watch</a></h5>
+			
+</div>
+		</div>
+
+		<div class="vbox wb_container" id="wb_main">
+
+			
+<div class="wb_page" id="page_1">
+				<a name="main" class="wb_page_anchor" id="page_1_anchor"></a>
+			<div id="wb_element_instance4" class="wb_element wb-elm-orient-horizontal">
+				<div class="wb-elm-line"></div>
+			</div>
+			<div id="wb_element_instance5" class="wb_element" style=" line-height: normal;">
+				<a name="characteristic" class="wb_anchor" style="top: -200px"></a>
+				<h1 class="wb-stl-heading1" style="text-align: center;"><strong>ХАРАКТЕРИСТИКА ЧАСОВ</strong></h1>
+			
+</div>
+			<div id="wb_element_instance6" class="wb_element" style=" line-height: normal;">
+				<p class="wb-stl-normal">Азиатский бренд теперь и в Украине. Это не просто современные, но и брендовые часы. Помимо, что мужские часы выглядят очень стильно благодаря металлическому дизайну, они также показывают электронное время и стрелочное! В них сочетается кварцевая классика с LED современностью. Такие часы отлично смотрятся под повседневный стиль, а полуавтоматическая застежка крепко держит их на руке.</p>
+
+			</div>
+			<div id="wb_element_instance7" class="wb_element" style=" line-height: normal;">
+				<p class="wb-stl-normal">От других часов такого рода данную новинку отличает крепкий нержавеющий корпус, надёжное стекло Hardlex, удобный браслет, водонепроницаемость, светодиодный циферблат и множество других полезных функций. Долгое время о таких часах можно было только мечтать... И только сейчас, благодаря смелым идеям дизайнеров и умелой работе часовых мастеров, они стали реальностью.</p>
+
+			</div>
+			<div id="wb_element_instance9" class="wb_element" style=" line-height: normal;">
+				<p class="wb-stl-normal" style="text-align: center;">Мужские водонепроницаемые часы TVG 468 - новинка от именитого часового производителя компании TVG WATCH.</p>
+
+			</div>
+			<div id="wb_element_instance10" class="wb_element">
+				<a class="wb_button" href=""><span>ЗАКАЗАТЬ СЕЙЧАС</span></a>
+			</div>
+			<div id="wb_element_instance11" class="wb_element wb_element_shape">
+				<div class="wb_shp"></div>
+			</div>
+			<div id="wb_element_instance12" class="wb_element wb_element_shape">
+				<div class="wb_shp"></div>
+			</div>
+			<div id="wb_element_instance13" class="wb_element" style=" line-height: normal;">
+				<a name="description" class="wb_anchor" style="top: -200px"></a>
+				<h1 class="wb-stl-heading1" style="text-align: center;"><strong>ОПИСАНИЕ</strong></h1>
+			
+</div>
+			<div id="wb_element_instance14" class="wb_element wb_element_picture">
+				<img alt="gallery/tvg_cs" src="gallery_gen//81a03d2a398fdf15b2f2f07b29578dc1_1200x540.jpg">
+			</div>
+			<div id="wb_element_instance15" class="wb_element wb_gallery">
+
+
+
+	<script type="text/javascript" src="js/WB_Gallery.class.js"></script>
+	<script type="text/javascript">
+				
+		$(function() {
+					
+		new WB_Gallery({"id":"wb_element_instance15","type":"slideshow","interval":10,"speed":400,"width":440,"height":440,"border":{"color":"#FFFFFF","style":"solid","weight":5,"radius":null,"css":{"border":"5px solid #FFFFFF"}},"padding":5,"thumbWidth":64,"thumbHeight":64,"images":[{"thumb":"gallery_gen\/9764c3403f71c8c22a3fd22bc9c36b14_gallery.jpg.thumb.jpg","image":"gallery_gen\/9764c3403f71c8c22a3fd22bc9c36b14_gallery.jpg","width":800,"height":800},{"thumb":"gallery_gen\/b905397410c148215a94daa48454c6b3_gallery.jpg.thumb.jpg","image":"gallery_gen\/b905397410c148215a94daa48454c6b3_gallery.jpg","width":500,"height":500},{"thumb":"gallery_gen\/2da187e6f837e35a92f7a36d333cee31_gallery.jpg.thumb.jpg","image":"gallery_gen\/2da187e6f837e35a92f7a36d333cee31_gallery.jpg","width":600,"height":600}]});
+
+	});
+
+	</script>
+
+
+			</div>
+			<div id="wb_element_instance16" class="wb_element wb-elm-orient-horizontal">
+				<div class="wb-elm-line"></div>
+			</div>
+		</div>
+		<div id="wb_element_instance17" class="wb_element" style="width: 100%;">
+
+			<?php
+				global $show_comments;
+				
+				if (isset($show_comments) && $show_comments) {
+					
+					renderComments(1);
+			
+			?>
+	
+
+		
+		<script type="text/javascript">
+				
+			$(function() {
+					
+			var block = $("#wb_element_instance17");
+					
+			var comments = block.children(".wb_comments").eq(0);
+					
+			var contentBlock = $("#wb_main");
+					
+			contentBlock.height(contentBlock.height() + comments.height());
+				
+		});
+			
+		</script>
+			
+
+		<?php
+				} 
+			else {
+			
+		?>
+			
+
+		<script type="text/javascript">
+				
+			$(function() {
+					
+			$("#wb_element_instance17").hide();
+				
+			});
+			
+		</script>
+			
+
+		<?php
+				}
+			
+		?>
+			
+		</div>
+	</div>
+
+	<div class="vbox wb_container" id="wb_footer" style="height: 124px;">
+
+		<div id="wb_element_instance3" class="wb_element" style=" line-height: normal;">
+			<p class="wb-stl-footer" style="text-align: center;">© 2017 <a href="http://army-watch.com.ua">army-watch.com.ua</a></p>
+		
+</div>
+		<div id="wb_element_instance18" class="wb_element" style="text-align: center; width: 100%;">
+			<div class="wb_footer"></div>
+
+
+	<script type="text/javascript">
+			
+		$(function() {
+				
+		var footer = $(".wb_footer");
+				
+		var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				
+		if (!html) {
+					
+		footer.parent().remove();
+					
+		footer = $("#wb_footer");
+					
+		footer.css({height: ""});
+				}
+			
+		});
+			
+		</script>
+		
+
+			</div>
+		</div>
+		<div class="wb_sbg"></div>
+	</div>{{hr_out}}
+</body>
+
+</html>
---
+++ blog.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Blog</title>
+	<base href="{{base_url}}" />
+			<meta name="viewport" content="width=992" />
+		<meta name="description" content="" />
+	<meta name="keywords" content="Blog" />
+		
+	<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<script src="js/jquery-1.11.3.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js" type="text/javascript"></script>
+	<script src="js/main.js?v=1.0.8" type="text/javascript"></script>
+
+	<link href="css/site.css?v=1.1.51" rel="stylesheet" type="text/css" />
+	<link href="css/common.css?ts=1499618196" rel="stylesheet" type="text/css" />
+	<link href="css/blog.css?ts=1499618196" rel="stylesheet" type="text/css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=0"/>
+<meta name="description" content="Часы TVG KM-468. Характеристики, фотографии, отзывы. Купить в Киеве с доставкой."/>
+<meta name="keywords" content="TVG, KM-468, наручные часы ТГВ, LED часы TGV, армейские часы"/>
+	<script type="text/javascript">var currLang = '';</script>		
+	<!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+	<!--[if lt IE 9]>
+	  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+</head>
+
+
+<body>{{ga_code}}<div class="root"><div class="vbox wb_container" id="wb_header">
+	
+<div id="wb_element_instance23" class="wb_element wb-menu"><ul class="hmenu menu-landing"><li><a href="#%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F" target="_self" title="Главная">Главная</a></li><li><a href="#description" target="_self" title="Описание">Описание</a></li><li><a href="#characteristic" target="_self" title="Характеристика">Характеристика</a></li></ul><div class="clearfix"></div></div><div id="wb_element_instance24" class="wb_element wb_element_picture"><a href="#%D0%93%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F"><img alt="gallery/m" src="gallery_gen//ec48dd153a7011d1873cef7e408dedde_100x100.png"></a></div></div>
+<div class="vbox wb_container" id="wb_main">
+	
+<div id="wb_element_instance26" class="wb_element" style="width: 100%;">
+			<?php
+				global $show_comments;
+				if (isset($show_comments) && $show_comments) {
+					renderComments(blog);
+			?>
+			<script type="text/javascript">
+				$(function() {
+					var block = $("#wb_element_instance26");
+					var comments = block.children(".wb_comments").eq(0);
+					var contentBlock = $("#wb_main");
+					contentBlock.height(contentBlock.height() + comments.height());
+				});
+			</script>
+			<?php
+				} else {
+			?>
+			<script type="text/javascript">
+				$(function() {
+					$("#wb_element_instance26").hide();
+				});
+			</script>
+			<?php
+				}
+			?>
+			</div></div>
+<div class="vbox wb_container" id="wb_footer" style="height: 124px;">
+	
+<div id="wb_element_instance25" class="wb_element" style=" line-height: normal;"><p class="wb-stl-footer" style="text-align: center;">© 2017 <a href="http://army-watch.com.ua">army-watch.com.ua</a></p>
+</div><div id="wb_element_instance27" class="wb_element" style="text-align: center; width: 100%;"><div class="wb_footer"></div><script type="text/javascript">
+			$(function() {
+				var footer = $(".wb_footer");
+				var html = (footer.html() + "").replace(/^\s+|\s+$/g, "");
+				if (!html) {
+					footer.parent().remove();
+					footer = $("#wb_footer");
+					footer.css({height: ""});
+				}
+			});
+			</script></div></div><div class="wb_sbg"></div></div>{{hr_out}}</body>
+</html>
